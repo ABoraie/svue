dc43bbb80c495aa27a61f6cd4b0843d4a393818e
ABoraie <alexanderboraie@hotmail.com>
2021-03-13 19:30:49 +0100

    Added init files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HeaderMenu />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import HeaderMenu from "./components/HeaderMenu.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    HeaderMenu
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="headermenu">
+        <!-- {{ msg }} -->
+        <a href="#home">Home</a>
+        <div class="submenu">
+            <button class="submenubtn">Stocks</button>
+            <div class="submenucontent">
+                <a href="#placeholder">Placeholder</a>
+                <a href="#placeholder">Placeholder</a>
+            </div>
+        </div>
+        <div class="submenu">
+            <button class="submenubtn">About</button>
+            <div class="submenucontent">
+                <a href="#placeholder">placeholder</a>
+                <a href="#placeholder">placeholder</a>
+            </div>
+        </div>
+        <div class="submenu">
+            <button class="submenubtn">Contribute</button>
+            <div class="submenucontent">
+                <a href="#placeholder">placeholder</a>
+                <a href="#placeholder">placeholder</a>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "HeaderMenu",
+  data: function () {
+      return {
+          msg: "hej"
+      }
+  },
+  created: function() {
+    console.log("HEj")
+  }
+});
+</script>
+
+<style>
+@import "../assets/main.css";
+</style>
